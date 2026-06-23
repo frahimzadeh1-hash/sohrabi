14e230a216df6517b0c141d6d28da3ba1f873548
frahimzadeh1-hash <f.rahimzadeh1@gmail.com>
2026-06-23 15:44:27 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 import json
 import os
+import requests
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sohrabi-campaign-2026'
